a1a8df8f793e9a86bdea9a4edfaaef7b3331ddbb
Matthew Thomas <matthew.thomas@rakuten.com>
2015-12-08 11:23:23 -0800

    Extract catch logic for logging in to the Azure Resource Manager in to its own function for maintainability.

--- Source/Scripts/CI.Azure.Functions.ps1
+++ Source/Scripts/CI.Azure.Functions.ps1
@@ -30,45 +30,60 @@ function GetOrLogin-AzureRmContext {
             throw
         }
 
-        if ($AzureDeployUser -and $AzureDeployPass) {
-            $AzureDeployPassSecure = $AzureDeployPass | ConvertTo-SecureString -AsPlainText -Force
-            $AzureCredential       = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AzureDeployUser,$AzureDeployPassSecure
-
-            $context = Login-AzureRmAccount -Credential $AzureCredential
-        } else {
-            Write-Host "Could not find any Azure credentials on the local machine, prompting console user..."
-            # Prompt the console user for credentials
-            $context = Login-AzureRmAccount
-        }
-
-        if (-not $context){
-            throw 'No Azure account found or specified!'
-        }
-
-        # Check to see if multiple subscriptions exist.
-        $subscriptions = Get-AzureRmSubscription
-
-        if ($subscriptions.Length -gt 1) {
-            Write-Host
-            Write-Host "Multiple Azure Subscriptions found:"
-            Write-Host
-
-            for($i = 0; $i -le $subscriptions.Length - 1; $i++) {
-                Write-Host "${i}: $($subscriptions[$i].SubscriptionName)"
-            }
-
-            Write-Host
-            $selectedIndex = Read-Host -Prompt "Enter # to choose"
-
-            $selectedSubscriptionId = $subscriptions[$selectedIndex].SubscriptionId
-
-            Select-AzureRmSubscription -SubscriptionId $selectedSubscriptionId
-
-            Write-Host "Switched to subscription $($subscriptions[$selectedIndex].SubscriptionName) [$selectedSubscriptionId]"
-        }
+        $context = Login-AzureResourceManager `
+            -AzureDeployUser $AzureDeployUser `
+            -AzureDeployPass $AzureDeployPass
     }
 
     Write-Output $context
+}
+
+# Logs the console user in to Azure.
+function Login-AzureResourceManager {
+    Param(
+        $AzureDeployUser,
+        $AzureDeployPass
+    )
+
+    if ($AzureDeployUser -and $AzureDeployPass) {
+        $AzureDeployPassSecure = $AzureDeployPass | ConvertTo-SecureString -AsPlainText -Force
+        $AzureCredential       = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AzureDeployUser,$AzureDeployPassSecure
+
+        $context = Login-AzureRmAccount -Credential $AzureCredential
+    } else {
+        Write-Host "Could not find any Azure credentials on the local machine, prompting console user..."
+        # Prompt the console user for credentials
+        $context = Login-AzureRmAccount
+    }
+
+    if (-not $context){
+        throw 'No Azure account found or specified!'
+    }
+
+    # Check to see if multiple subscriptions exist.
+    $subscriptions = Get-AzureRmSubscription
+
+    if ($subscriptions.Length -gt 1) {
+        Write-Host
+        Write-Host "Multiple Azure Subscriptions found:"
+        Write-Host
+
+        for($i = 0; $i -le $subscriptions.Length - 1; $i++) {
+            Write-Host "${i}: $($subscriptions[$i].SubscriptionName)"
+        }
+
+        Write-Host
+        
+        $selectedIndex = Read-Host `
+            -Prompt "Enter # to choose"
+
+        $selectedSubscriptionId = $subscriptions[$selectedIndex].SubscriptionId
+
+        Select-AzureRmSubscription `
+            -SubscriptionId $selectedSubscriptionId
+
+        Write-Host "Switched to subscription $($subscriptions[$selectedIndex].SubscriptionName) [$selectedSubscriptionId]"
+    }
 }
 
 function Upload-FileToBlob {
