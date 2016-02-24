7a8a7b1de1803c2bc127d7ae6f8689b71f42dda6
Matthew Thomas <matthew.thomas@rakuten.com>
2016-02-24 01:26:12 -0800

    Testing Write-Error.

--- CI.ps1
+++ CI.ps1
@@ -4,5 +4,5 @@ $ciTasksPath = Join-Path $PSScriptRoot Source\Scripts\CI.Tasks.ps1
 . $psakePath $ciTasksPath $args -parameters @{BasePath=$PSScriptRoot} -nologo -framework 4.6
 
 if ($LASTEXITCODE -ne 0) {
-    throw "PSake failed with exit code $LASTEXITCODE.  See previous errors for details."
+    Write-Error "PSake failed with exit code $LASTEXITCODE.  See previous errors for details."
 }
