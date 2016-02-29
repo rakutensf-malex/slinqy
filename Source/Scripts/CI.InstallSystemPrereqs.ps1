a1fe5e78c9712c84b30d844520027b27c8f63987
Matthew Thomas <matthew.thomas@rakuten.com>
2016-02-29 14:13:20 -0800

    Removing CI code for installing PowerShell modules, not gonna work on the  hosted agent.

--- Source/Scripts/CI.InstallSystemPrereqs.ps1
+++ Source/Scripts/CI.InstallSystemPrereqs.ps1
@@ -21,27 +21,6 @@ function Exec
     }
 }
 
-function Add-Module 
-{
-    [CmdletBinding()]
-    param(
-        [Parameter(Position=0,Mandatory=1)][string]$Path
-    )
-
-	$Path = Resolve-Path $Path
-
-	#Save the current value in the $p variable.
-	$p = [Environment]::GetEnvironmentVariable("PSModulePath")
-
-	#Add the new path to the $p variable. Begin with a semi-colon separator.
-	$p += ";$Path"
-
-	#Add the paths in $p to the PSModulePath value.
-	[Environment]::SetEnvironmentVariable("PSModulePath", $p)
-
-	Write-Host "Added $Path to PSModulePath."
-}
-
 # Make sure all the system level prerequisites that require Admin rights are installed.
 
 $rootPath           = Join-Path $PSScriptRoot '..\..\'
@@ -49,33 +28,14 @@ $toolsPath          = Join-Path $rootPath     'Tools'
 $nugetPath          = Join-Path $toolsPath    'nuget.exe'
 $packagesConfigPath = Join-Path $toolsPath    'packages.config'
 $packagesDirectory  = Join-Path $toolsPath    'packages'
-$modulesPath        = Join-Path $toolsPath    'PowerShellModules\'
 
 exec { . $nugetPath restore $packagesConfigPath -packagesDirectory $packagesDirectory }
 
-Write-Host "Adding CI PowerShell modules to PSModulePath..."
+Write-Host "PowerShell:   $($PSVersionTable.PSVersion)"
+Write-Host "PSModulePath: ${env:PSModulePath}"
+Write-Host
+Write-Host "Available Modules:"
 
-Add-Module -Path $modulesPath
-
-	# TODO: REMOVE
-	Write-Host "PowerShell:"
-	Write-Host $PSVersionTable.PSVersion
-	Write-Host "PSModulePath: ${env:PSModulePath}"
-	Write-Host "Available Modules:"
-	foreach ($module in Get-Module -ListAvailable) {
-		Write-Host $module.Name $module.Version
-	}
-
-Write-Host 'Loading PowerShell modules...'
-
-Import-Module PackageManagement -RequiredVersion 1.0.0.0
-Import-Module PowerShellGet     -RequiredVersion 1.0
-
-$azureModulePath = Join-Path $modulesPath 'Azure'
-if (-not (Test-Path $azureModulePath)) {
-	Save-Module `
-		-Name Azure `
-		-RequiredVersion 1.0.4 `
-		-Path $modulesPath `
-		-MinimumVersionForce
+foreach ($module in Get-Module -ListAvailable) {
+	Write-Host $module.Name $module.Version
 }
