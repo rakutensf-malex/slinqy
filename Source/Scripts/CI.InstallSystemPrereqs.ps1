b43fdd92ab9fc1af32b1aadacf0ce2e29a59bca2
Matthew Thomas <matthew.thomas@rakuten.com>
2016-03-02 19:01:28 -0800

    Install AzureRM.profile module.

--- Source/Scripts/CI.InstallSystemPrereqs.ps1
+++ Source/Scripts/CI.InstallSystemPrereqs.ps1
@@ -71,6 +71,21 @@ if (-not $azureCmdletsInstalled) {
 	Write-Host 'done!'
 }
 
+$azureCmdletsInstalled = Is-ModuleInstalled `
+	-Name    'AzureRM.profile' `
+	-Version '1.0.4'
+
+if (-not $azureCmdletsInstalled) {
+	Write-Host 'Installing AzureRM.profile 1.0.4...' -NoNewline
+
+	Install-Package `
+		-Name            'AzureRM.profile' `
+		-RequiredVersion '1.0.4' `
+		-Force
+
+	Write-Host 'done!'
+}
+
 # Install CI dependencies that don't require Admin rights
 exec { . $nugetPath restore $packagesConfigPath -packagesDirectory $packagesPath }
 
